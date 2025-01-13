c21f97ebab0f2ad02e3e99ec5d3e0cb1654563a7
Joel Laxamana <laxamanj@gene.com>
2025-01-13 20:16:22 +0000

    including pilot3 project to help utilize metadata to generate R adrg guidance.

---
+++ pilot3/set-paths.r
@@ -0,0 +1,5 @@
+path <- list(
+  sdtm = "./pilot3/m5/datasets/rconsortiumpilot3/tabulations/sdtm",
+  adam = "./pilot3/m5/datasets/rconsortiumpilot3/analysis/adam-reviewer/datasets",
+  output = "./pilot3/m5/datasets/rconsortiumpilot3/analysis/output"
+)
